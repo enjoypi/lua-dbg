bb64c7e3685facee80ebe3a69e282945459ffa17
zfbp <zzffbbpp@gmail.com>
2015-05-28 13:25:21 +0800

    test.lua

--- test.lua
+++ test.lua
@@ -1,9 +1,16 @@
 local d = require("debugger")
 
+
+function g(...)
+	local a = 1999
+	print(a)
+end
+
 function f( ... )
 	print(coroutine.running())
 	d.Break()
 	require("test_inner").f()
+	g(...)
 	-- body
 end
 
